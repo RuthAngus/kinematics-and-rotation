dc1199c3067ad81b441d141ad350c1c8960438b6
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2019-08-29 13:48:17 -0400

    trying to calculate v_l

--- calc_velocities.py
+++ calc_velocities.py
@@ -35,5 +35,54 @@ def calc_vb(pandas_df, nsamples=1000):
     pm_b = np.array(vels.pm_b.value)
 
     gal = c_samples.galactic
-    v_b = (gal.pm_b * gal.distance).to(u.km/u.s, u.dimensionless_angles())
+    v_b = (gal.pm_l * gal.distance).to(u.km/u.s, u.dimensionless_angles())
     return pm_b, np.array(v_b.value)
+
+
+def calc_vl(pandas_df, nsamples=1000):
+    """
+    Calculate velocity samples in the l direction from a pandas DataFrame.
+
+    Args:
+        df (DataFrame): pandas dataframe containing Gaia columns
+
+    Returns:
+        pm_l (array): samples of velocity in the l direction.
+            Shape = nstars x nsamples
+
+    """
+
+    df = Table.from_pandas(pandas_df)
+    g = GaiaData(df)
+    g_samples = g.get_error_samples(size=nsamples,
+                                    rnd=np.random.RandomState(seed=42))
+    c_samples = g_samples.get_skycoord()
+    vels = c_samples.transform_to(coord.Galactic)
+    # pm_l = np.array(vels.pm_l.value)
+
+    gal = c_samples.galactic
+    v_l = (gal.pm_l * gal.distance).to(u.km/u.s, u.dimensionless_angles())
+    return np.array(v_l.value)
+
+
+def calc_vz(pandas_df, nsamples=1000):
+    """
+    Calculate W samples from a pandas DataFrame.
+
+    Args:
+        df (DataFrame): pandas dataframe containing Gaia columns
+
+    Returns:
+        W (array): samples of W velocity.
+            Shape = nstars x nsamples
+
+    """
+
+    df = Table.from_pandas(pandas_df)
+    g = GaiaData(df)
+    g_samples = g.get_error_samples(size=nsamples,
+                                    rnd=np.random.RandomState(seed=42))
+    c_samples = g_samples.get_skycoord()
+    vels = c_samples.transform_to(coord.Galactocentric)
+    W = vels.v_z.value
+    return np.mean(W, axis=1), np.std(W, axis=1)
